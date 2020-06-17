6cf0d9936676161769c8057ddb7a703cc6e912a0
[csbbbv] <[936357225@qq.com]>
2020-06-17 14:58:58 +0800

    提交一个为图片建立哈希值方法

--- img_hash.py
+++ img_hash.py
@@ -2,13 +2,16 @@ import hashlib
 from PIL import Image
 import numpy as np
 
-def img_hash(strA):
-    fd1 = np.array(Image.open(strA)) * np.random.randint(10)
+def img_hash(str):
+    fd1 = np.array(Image.open(str))
     return hashlib.md5(fd1).hexdigest()
 
 
 if __name__ == '__main__':
-    imageA = 'G:\\test\\style_test\\常玉.jpg'
-    imageB = 'G:\\test\\style_test\\常玉2.jpg'
-    res = img_hash(imageA)
-    print(res)
+    imageA = 'G:\\test\\style_test\\呐喊1.jpg'
+    imageB = 'G:\\test_multi_pics2\\cliff4\\呐喊1.jpg'
+    imageC = 'G:\\test\\style_test\\呐喊2.jpg'
+    res1 = img_hash(imageA)
+    res2 = img_hash(imageB)
+    res3 = img_hash(imageC)
+    print(res1 == res2, res1 == res3, res2 == res3)
